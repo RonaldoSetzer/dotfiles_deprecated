6fe9858b40eb3c9f0e4c9e6b5630251871c91956
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-10-25 23:15:40 -0300

    Chore(emacs): Setup Evil mode

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,6 +18,26 @@
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode 1)
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
 (setq inhibit-startup-message t)
 
 (scroll-bar-mode -1)        ; Disable visible scrollbar
