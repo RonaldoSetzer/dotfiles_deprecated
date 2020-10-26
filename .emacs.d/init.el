fd9187d489531476a25701d46752f362fdd20e1f
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-10-26 12:04:04 -0300

    Chore(emacs): Improve completions with ivy

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -66,6 +66,35 @@
 
 (set-face-attribute 'default nil :font "Operator Mono Lig" :height 160)
 
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :init
+  (ivy-mode 1))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package counsel
+  :bind (("C-M-j" . 'counsel-switch-buffer)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (counsel-mode 1))
+
 (org-babel-do-load-languages
   'org-babel-load-languages
   '((emacs-lisp . t)
