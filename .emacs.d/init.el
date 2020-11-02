eb2297227103fe385b07240b61e699f74cf561b5
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2020-11-02 00:27:41 -0300

    Chore(emacs): Add Git support with magit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -119,6 +119,29 @@
   :config
   (counsel-mode 1))
 
+(use-package magit
+  :commands (magit-status magit-get-current-branch)
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package evil-magit
+  :after magit)
+
+(rs/leader-keys
+  "g"   '(:ignore t :which-key "git")
+  "gs"  'magit-status
+  "gd"  'magit-diff-unstaged
+  "gc"  'magit-branch-or-checkout
+  "gl"   '(:ignore t :which-key "log")
+  "glc" 'magit-log-current
+  "glf" 'magit-log-buffer-file
+  "gb"  'magit-branch
+  "gP"  'magit-push-current
+  "gp"  'magit-pull-branch
+  "gf"  'magit-fetch
+  "gF"  'magit-fetch-all
+  "gr"  'magit-rebase)
+
 (use-package lsp-mode
   :commands (lsp lsp-deferred)
   :init
