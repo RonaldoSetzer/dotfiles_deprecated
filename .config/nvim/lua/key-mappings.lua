c731e2ae2758afba048147ef195ad590e88e0826
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2021-03-19 00:31:24 -0300

    feat: Map jk, jj, kj to back to normal mode

--- .config/nvim/lua/key-mappings.lua
+++ .config/nvim/lua/key-mappings.lua
@@ -1,3 +1,8 @@
 -- Map for LeaderKey key
 vim.api.nvim_set_keymap('n', '<Space>', '<NOP>', { noremap = true, silent = true })
 vim.g.mapleader = ' '
+
+-- Map jk, jj, kj for exit to normal mode
+vim.api.nvim_set_keymap('i', 'jk', '<Esc>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('i', 'kj', '<Esc>', { noremap = true, silent = true })
