598285b44d5247569e968faaa0134216bda2bdc2
Ronaldo Santiago <ronaldo.setzer@gmail.com>
2021-03-23 00:00:21 -0300

    feat: Improve compe config

--- .config/nvim/lua/plugins-config/nvim-compe.lua
+++ .config/nvim/lua/plugins-config/nvim-compe.lua
@@ -1,18 +1,7 @@
 vim.o.completeopt = "menuone,noselect"
 
 require'compe'.setup {
-  enabled = true;
-  autocomplete = true;
-  debug = false;
-  min_length = 1;
-  preselect = 'enable';
-  throttle_time = 80;
-  source_timeout = 200;
-  incomplete_delay = 400;
-  max_abbr_width = 100;
-  max_kind_width = 100;
-  max_menu_width = 100;
-  documentation = true;
+  preselect = 'always';
 
   source = {
     path = true;
@@ -65,3 +54,4 @@ vim.api.nvim_set_keymap("i", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<Tab>", "v:lua.tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("i", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
 vim.api.nvim_set_keymap("s", "<S-Tab>", "v:lua.s_tab_complete()", {expr = true})
+vim.api.nvim_set_keymap("i","<CR>", "compe#confirm(\'<CR>\')", { expr =true, silent=true })
